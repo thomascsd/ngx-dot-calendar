37552071bd1503db48a86840eb3859d2d29fdbe1
thomascsd <thomascsd808@gmail.com>
2023-06-25 09:58:57 +0800

    add header component on web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule} from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NgxDotCalendarModule } from 'ngx-dot-calendar';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule, NgxDotCalendarModule],
   providers: [],
   bootstrap: [AppComponent]
